7a52497e4f63b9c662bde4d6ac3ada25d84262e4
alifia256 <alifiaaazzahra28@gmail.com>
2026-06-10 16:10:49 +0700

    fix: increase price and total_sales column size

---
+++ database/migrations/2026_06_10_090542_fix_price_column_in_sales_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->decimal('price', 15, 2)->change();
+            $table->decimal('total_sales', 15, 2)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->decimal('price', 8, 2)->change();
+            $table->decimal('total_sales', 8, 2)->change();
+        });
+    }
+};
