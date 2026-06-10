c33e2d90da0f28064fbc416a7fdfb5fa02e47c22
alifia256 <alifiaaazzahra28@gmail.com>
2026-06-10 14:58:05 +0700

    fix: pake salesModel

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use App\Models\Sale; // Asumsi nama modelmu adalah Sale atau ganti menjadi Transaction jika namamu itu
+use App\Models\SalesModel;
 
 class AdminDashboardController extends Controller
 {
